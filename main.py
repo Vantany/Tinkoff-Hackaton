60f3ddcfa9bf42fd09c305200e0cad666712828a
Vantany <v1pov2005@yandex.ru>
2023-11-16 22:06:23 +0300

    new bug fix

--- main.py
+++ main.py
@@ -19,18 +19,14 @@ SESSION_ID = os.getenv("SESSION_ID")
 
 
 def registerBotInSessionResponse():
-    global bot_id, password
-    mediator_url = os.getenv("MEDIATOR_URL")
-    session_id = os.getenv("SESSION_ID")
-    bot_url = os.getenv("BOT_URL")
-    requests.post(f"{mediator_url}/sessions/{session_id}/registration",
-                  {"bot_id": bot_id, "password": password, "bot_url": bot_url})
+    global BOT_URL, BOT_ID, PASSWORD, MEDIATOR_URL, SESSION_ID
+    requests.post(f"{MEDIATOR_URL}/sessions/{SESSION_ID}/registration",
+                  {"bot_id": BOT_ID, "password": PASSWORD, "bot_url": BOT_URL})
 
 
 def registerBotInSessionRequest():
-    mediator_url = os.getenv("MEDIATOR_URL")
-    session_id = os.getenv("SESSION_ID")
-    return requests.get(f"{mediator_url}/sessions/{session_id}/registration").json()["figure"]
+    global MEDIATOR_URL, SESSION_ID
+    return requests.get(f"{MEDIATOR_URL}/sessions/{SESSION_ID}/registration").json()["figure"]
 
 
 @app.route("/bot/turn", methods = ["POST", "GET"])
